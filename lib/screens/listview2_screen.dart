1179b22c495403bbbbd4c7d85bc0d38b3d754669
Alan Ibarra <aledibarralo@ittepic.edu.mx>
2022-06-27 00:28:16 -0600

    Feat: Listview separated

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Listview Tipo 2'),
+        ),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            leading: const Icon(Icons.access_time_sharp),
+            title: Text(options[index]),
+            trailing: const Icon(Icons.arrow_forward_ios_sharp),
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ));
+  }
+}

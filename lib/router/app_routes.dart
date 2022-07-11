83938e0c807d31165c475fc80e77f3b98187bd61
Alan Ibarra <aledibarralo@ittepic.edu.mx>
2022-07-10 23:36:42 -0600

    Feat: add menu_options model.

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption> [
+    MenuOption(route: 'home', icon: Icons.home, name: 'home', screen: const HomeScreen()),
+    MenuOption(route: 'listview1', icon: Icons.list, name: 'Listview tipo 1', screen: const Listview1Screen()),
+    MenuOption(route: 'listview2', icon: Icons.list_outlined, name: 'Listview tipo 2', screen: const Listview2Screen()),
+    MenuOption(route: 'alert', icon: Icons.add_alert, name: 'Alertas -Alerts', screen: const AlertScreen()),
+    MenuOption(route: 'card', icon: Icons.card_giftcard, name: 'Tarjetas - Cards', screen: const CardScreen()),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext buildContext) => const HomeScreen(),
